3d365b97c36643f2b046d19177ee1be52f4300c8
GastonE-Dev <gedev1988@gmail.com>
2023-06-17 13:25:29 -0300

    Added attribute to Restaurant class to indicate # of customers served, added functionality to increment # of customers served by 5, added login attempts count to User class

--- Restaurant-oop.py
+++ Restaurant-oop.py
@@ -7,6 +7,10 @@ class Restaurant:
         self.restaurant_name = restaurant_name
         self.cuisine_type = cuisine_type
 
+        # Added the following attribute to indicate the default number of customers served
+
+        self.number_served = 0
+
     def describe_restaurant(self):
         """Describes the restaurant attributes"""
         print("Restaurant name:", self.restaurant_name)
@@ -21,6 +25,17 @@ class Restaurant:
         """Simulate the closing of the restaurant"""
         print("This restaurant has closed for today")
 
+        # Added the following method to set the number of customers served in each object/instance
+
+    def set_number_served(self):
+        """Set number of customers that have been served"""
+        print("This restaurant has served " + str(self.number_served) + " customers.")
+
+    def increment_number_served(self):
+        """Increment number of customers that were served in a day of business"""
+        self.number_served += 5
+
+
 
 # Create an instance of the Restaurant class
 
@@ -29,6 +44,9 @@ restaurant = Restaurant('La internacional', 'International')
 print("This restaurant's name is " + restaurant.restaurant_name + ".")
 print("This restaurant serves " + str(restaurant.cuisine_type) + ' food')
 restaurant.open_restaurant()
+restaurant.set_number_served()
+restaurant.increment_number_served()
+restaurant.set_number_served()
 
 
 print()
@@ -38,7 +56,8 @@ restaurant2 = Restaurant("Rup's Kitchen", 'Indian')
 
 print("This other restaurant is called " + restaurant2.restaurant_name + ".")
 print("They serve " + str(restaurant2.cuisine_type) + " food. ")
-restaurant.close_restaurant()
+restaurant2.set_number_served()
+restaurant2.close_restaurant()
 
 
 print()
@@ -49,20 +68,41 @@ print()
 
 restaurant3 = Restaurant('Asado Argento', 'Argentina!')
 restaurant3.describe_restaurant()
+restaurant3.set_number_served()
+restaurant3.increment_number_served()
+restaurant3.set_number_served()
+restaurant3.increment_number_served()
+restaurant3.set_number_served()
+restaurant3.set_number_served()
+restaurant3.increment_number_served()
+restaurant3.set_number_served()
+restaurant3.close_restaurant()
+
 
 
 print()
 
 restaurant4 = Restaurant('Ye olde Americana', 'American')
 restaurant4.describe_restaurant()
+restaurant4.open_restaurant()
+restaurant4.set_number_served()
+restaurant4.increment_number_served()
+restaurant4.set_number_served()
+restaurant4.close_restaurant()
 
 print()
 
 restaurant5 = Restaurant('The Great Wall', 'Chinese')
 restaurant5.describe_restaurant()
-
+restaurant5.open_restaurant()
+restaurant5.set_number_served()
+restaurant5.increment_number_served()
+restaurant5.set_number_served()
+restaurant5.increment_number_served()
+restaurant5.set_number_served()
 
 print()
+
 
 # Added the following class defining users by attribute
 # and added a personalized greeting for each
@@ -79,6 +119,18 @@ class User:
         self.first_name = first_name
         self.last_name = last_name
         self.dob = dob
+        # Added the following attribute and methods later
+
+        self.login_attempts = 0
+
+    def increment_login_attempts(self):
+        """Increment the value of login attempts by 1"""
+        self.login_attempts += 1
+
+    def reset_login_attempts(self):
+        """Reset value of login attempts to 0"""
+        self.reset_login_attempts = 0
+
 
     def describe_user(self):
         """This method prints a summary of users's information"""
@@ -92,9 +144,23 @@ class User:
         print('Welcome Mr/Mrs ' + self.first_name + ' ' + self.last_name + '!' ' please check our list of restaurants. ')
 
 
+
+
 user1 = User('101100', 'Bruce', 'Wayne', '08-25-1988')
 user1.describe_user()
 user1.greet_user()
+user1.increment_login_attempts()
+print('This user has ' + str(user1.login_attempts) + ' login attempts.')
+user1.increment_login_attempts()
+user1.increment_login_attempts()
+user1.increment_login_attempts()
+user1.increment_login_attempts()
+user1.increment_login_attempts()
+user1.increment_login_attempts()
+print('This user has ' + str(user1.login_attempts) + ' login attempts.')
+user1.reset_login_attempts()
+print('Current login attempts ' + str(user1.reset_login_attempts) )
+
 
 print()
 
@@ -102,12 +168,24 @@ print()
 user2 = User('101200', 'Commander', 'Shepard', '03-25-2140')
 user2.describe_user()
 user2.greet_user()
+user2.increment_login_attempts()
+user2.increment_login_attempts()
+user2.increment_login_attempts()
+user2.increment_login_attempts()
+print('This user has ' + str(user2.login_attempts) + ' login attempts.')
+user2.reset_login_attempts()
+print('Current login attempts ' + str(user2.reset_login_attempts) )
 
 print()
 
 user3 = User('101300', 'Tony', 'Stark', '05-29-1970')
 user3.describe_user()
 user3.greet_user()
+user3.increment_login_attempts()
+user3.increment_login_attempts()
+print('This user has ' + str(user3.login_attempts) + ' login attempts.')
+user3.reset_login_attempts()
+print('Current login attempts ' + str(user3.reset_login_attempts) )
 
 
 print()
@@ -116,3 +194,10 @@ print()
 user4 = User('101400', 'Mark', 'Smith', '06-12-2000')
 user4.describe_user()
 user4.greet_user()
+user4.increment_login_attempts()
+user4.increment_login_attempts()
+user4.increment_login_attempts()
+user4.increment_login_attempts()
+print('This user has ' + str(user4.login_attempts) + ' login attempts.')
+user4.reset_login_attempts()
+print('Current login attempts ' + str(user4.reset_login_attempts) )
