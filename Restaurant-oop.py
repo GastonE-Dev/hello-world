5bce0d65d94f7010c112609db8fb35a3fd26dd19
GastonE-Dev <gedev1988@gmail.com>
2023-06-30 12:20:06 -0300

    added new child class to Restaurant, added new child class to User

--- Restaurant-oop.py
+++ Restaurant-oop.py
@@ -145,31 +145,40 @@ class User:
 
 user1 = User('101100', 'Bruce', 'Wayne', '08-25-1988')
 user1.describe_user()
-user1.greet_user()
-user1.increment_login_attempts()
-print('This user has ' + str(user1.login_attempts) + ' login attempts.')
 user1.increment_login_attempts()
 user1.increment_login_attempts()
 user1.increment_login_attempts()
 user1.increment_login_attempts()
 user1.increment_login_attempts()
 user1.increment_login_attempts()
-print('This user has ' + str(user1.login_attempts) + ' login attempts.')
-user1.reset_login_attempts()
-print('Current login attempts ' + str(user1.reset_login_attempts) )
-
+user1.increment_login_attempts()
+print('User has ' + str(user1.login_attempts) + ' login attempts.')
 
 print()
 
+user1.greet_user()
+
+print()
+
+user1.reset_login_attempts()
+print('Current login attempts ' + str(user1.reset_login_attempts) )
+
+print()
 
 user2 = User('101200', 'Commander', 'Shepard', '03-25-2140')
 user2.describe_user()
+user2.increment_login_attempts()
+user2.increment_login_attempts()
+user2.increment_login_attempts()
+user2.increment_login_attempts()
+print('User has ' + str(user2.login_attempts) + ' login attempts.')
+
+print()
+
 user2.greet_user()
-user2.increment_login_attempts()
-user2.increment_login_attempts()
-user2.increment_login_attempts()
-user2.increment_login_attempts()
-print('This user has ' + str(user2.login_attempts) + ' login attempts.')
+
+print()
+
 user2.reset_login_attempts()
 print('Current login attempts ' + str(user2.reset_login_attempts) )
 
@@ -177,24 +186,77 @@ print()
 
 user3 = User('101300', 'Tony', 'Stark', '05-29-1970')
 user3.describe_user()
+user3.increment_login_attempts()
+user3.increment_login_attempts()
+print('User has ' + str(user3.login_attempts) + ' login attempts.')
+
+print()
+
 user3.greet_user()
-user3.increment_login_attempts()
-user3.increment_login_attempts()
-print('This user has ' + str(user3.login_attempts) + ' login attempts.')
+
+print()
+
 user3.reset_login_attempts()
 print('Current login attempts ' + str(user3.reset_login_attempts) )
+
+print()
+
+user4 = User('101400', 'Mark', 'Smith', '06-12-2000')
+user4.describe_user()
+user4.increment_login_attempts()
+user4.increment_login_attempts()
+user4.increment_login_attempts()
+user4.increment_login_attempts()
+print('User has ' + str(user4.login_attempts) + ' login attempts.')
+
+print()
+
+user4.greet_user()
+
+user4.reset_login_attempts()
+print('Current login attempts ' + str(user4.reset_login_attempts) )
 
 
 print()
 
 
-user4 = User('101400', 'Mark', 'Smith', '06-12-2000')
-user4.describe_user()
-user4.greet_user()
-user4.increment_login_attempts()
-user4.increment_login_attempts()
-user4.increment_login_attempts()
-user4.increment_login_attempts()
-print('This user has ' + str(user4.login_attempts) + ' login attempts.')
-user4.reset_login_attempts()
-print('Current login attempts ' + str(user4.reset_login_attempts) )
+class IceCreamStand(Restaurant):
+    """Represents an ice cream stand, a specific kind of restaurant"""
+    def __init__(self, restaurant_name, cuisine_type, flavors):
+        super().__init__(restaurant_name, cuisine_type)
+        self.flavors = flavors
+
+Ice_cream_stand = IceCreamStand("Giulio's gelato", 'Ice cream shop', ['Rocky road', 'Creamy strawberry', 'Boring chocolate', 'Vanilla'])
+Ice_cream_stand.describe_restaurant()
+print('This shop serves the following flavors: ', Ice_cream_stand.flavors)
+
+print()
+
+Ice_cream_stand2 = IceCreamStand('Creamy goodness', 'Ice cream shop', ['Banana Split', 'Mint & chocolate', 'Marroc & vanilla'])
+Ice_cream_stand2.describe_restaurant()
+print('This shop serves the following flavors: ', Ice_cream_stand2.flavors)
+
+print()
+
+Ice_cream_stand3 = IceCreamStand('Cold desires', 'Ice cream shop', ['Tramontana', 'Caramel', 'Pecan chocolate', 'Butter pecan', 'Strawberry marmolade'])
+Ice_cream_stand3.describe_restaurant()
+print('This shop serves the following flavors: ', Ice_cream_stand3.flavors)
+
+print()
+print('The following users have administrator privileges:')
+
+class Admin(User):
+    """Represents a special kind of user, an admin"""
+    def __init__(self, user_id, first_name, last_name, dob, privileges):
+        super().__init__(user_id, first_name, last_name, dob)
+        self.privileges = privileges
+
+    def show_privileges(self):
+        """Display privileges"""
+        print(self.first_name + ' ' + self.last_name + ': ' + str(self.privileges))
+
+admin1 = Admin('101400', 'Mark', 'Smith', '06-12-2000', ['can add post','can delete post'])
+admin1.show_privileges()
+
+admin2 = Admin('101200', 'Commander', 'Shepard', '03-25-2140', ['can add post', 'can delete post', 'can ban user'])
+admin2.show_privileges()
